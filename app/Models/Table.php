1e0e7495a67508389aa40b285198ccb87bf7dca5
phumrapee <phumrapee.v@Kkumail.com>
2024-09-22 21:51:36 +0700

    use solf_delete in models

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Table extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function bill(){
         return $this->hasMany(Bill::class);
     }
 
+    public function stage(){
+
+    }
 }
