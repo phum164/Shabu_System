1e886de3693483ba023c87082fe1748038e6807e
phumrapee <phumrapee.v@Kkumail.com>
2024-09-25 22:23:54 +0700

    test bill

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Bill;
+use App\Models\Table;
 
 class BillController extends Controller
 {
@@ -14,24 +15,15 @@ class BillController extends Controller
     {
         //
         $bills = Bill::all();
-        return view('#',compact('bills'));
+        return view('#', compact('bills'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Request $request)
+    public function create()
     {
-        $time = date('Y-m-d H:i:s',time());
-        Bill::create([
-            'employee_id' => $request->empid,
-            'table_id' => $request->tableid,
-            'person_amount	' => $request->amount,
-            'total_pay' => $request->amount*299,
-            'status' => 0,
-            'start_time' => $time,
-            'end_time' => date('Y-m-d H:i:s', strtotime('+2 hours')),
-        ]);
+        return view('createbill');
     }
 
     /**
@@ -39,9 +31,20 @@ class BillController extends Controller
      */
     public function store(Request $request)
     {
-        $data = [
-
-        ];
+        $time = date('Y-m-d H:i:s', time());
+        $bill = Bill::create([
+            'employee_id' => $request->empid,
+            'table_id' => $request->tableid,
+            'person_amount	' => $request->amount,
+            'total_pay' => $request->amount * 299,
+            'status' => 0,
+            'start_time' => $time,
+            'end_time' => date('Y-m-d H:i:s', strtotime('+2 hours')),
+        ]);
+        Table::status($request->tableid, 0);
+        $request->session()->put('bill_id', $bill->id);
+        return view('testbill_section', ['billId' => $bill->id]);
+        // return redirect();
     }
 
     /**
@@ -50,7 +53,7 @@ class BillController extends Controller
     public function show($id)
     {
         $bill = Bill::find($id);
-        return view('#',compact('bill'));
+        return view('#', compact('bill'));
     }
 
     /**
@@ -61,7 +64,7 @@ class BillController extends Controller
         $bill = Bill::find($request->id);
         $bill::update([
             'person_amount	' => $bill->person_amount + $request->amount,
-            'total_pay' => $bill->person_amount + ($request->amount*299),
+            'total_pay' => $bill->person_amount + ($request->amount * 299),
         ]);
     }
 
@@ -73,11 +76,27 @@ class BillController extends Controller
         //
     }
 
+    public function checkbill($id)
+    {
+        $time = date('Y-m-d H:i:s', time());
+        $bill = Bill::find($id);
+        Table::status($bill->table_id, 0);
+        $bill->update([
+            'status' => 1,
+            'finish_time' => $time,
+        ]);
+    }
     /**
      * Remove the specified resource from storage.
      */
+    //check bill
     public function destroy(string $id)
     {
         //
+    }
+    public function showBill($billId)
+    {
+        $bill = Bill::findOrFail($billId);
+        return view('show-bill', compact('bill'));
     }
 }
