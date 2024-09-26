97b274f9bf5842b74941fa4f7af3657f8b9e3d9a
Sun <thornthan111@gmail.com>
2024-09-26 18:51:36 +0700

    หน้าพนักงานใหม่ หน้าแก้ไขใหม่

--- resources/views/editemp.blade.php
+++ resources/views/editemp.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/empdata.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -18,18 +20,42 @@
             <input type="text" placeholder="ค้นหา">
         </div>
     </header>
-    <form action="{{ route('/empdata', $employees->emp) }}" method="POST">
+
+    <form action="{{ route('/empdata', $employees->id) }}" method="POST">
         @csrf
         @foreach ($employees as $emp)
             <div class="emp">
-                <b class="emp-id"></b>
-                <input type="text" name="id" value="{{ $emp->id }}" class="form-control" readonly>
-                <p>ชื่อ-นามสกุล: </p>
-                <input type="text" name="name" value="{{ $emp->name }}" class="form-control">
-                <p>เงินเดือน: </p>
-                <input type="text" name="position_salary" value="{{ $emp->position->sarary }}" class="form-control">
-                <p>แผนก: </p>
-                <input type="text" name="position_name" value="{{ $emp->position->name }}" class="form-control">
+                <div class="form-group row align-items-center mb-3">
+                    <label for="id" class="col-sm-2 col-form-label">รหัสพนักงาน:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="id" id="id" value="{{ $emp->id }}"
+                            class="form-control" readonly>
+                    </div>
+                </div>
+
+                <div class="form-group row align-items-center mb-3">
+                    <label for="name" class="col-sm-2 col-form-label text-right">ชื่อ-นามสกุล:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="name" id="name"
+                            placeholder="{{ $emp->name }} class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group row align-items-center mb-3">
+                    <label for="position_salary" class="col-sm-2 col-form-label text-right">เงินเดือน:</label>
+                    <div class="col-sm-10">
+                        <input type="number" name="position_salary" id="position_salary"
+                            placeholder="{{ $emp->position->sarary }}" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group row align-items-center mb-3">
+                    <label for="position_name" class="col-sm-2 col-form-label">แผนก:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="position_name" id="position_name"
+                            placeholder="{{ $emp->position->name }}" class="form-control">
+                    </div>
+                </div>
 
                 <input type="submit" class="btn btn-primary" value="บันทึก">
                 <a href="{{ url()->previous() }}" class="btn btn-danger">ยกเลิก</a>
--- resources/views/empdata.blade.php
+++ resources/views/empdata.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/empdata.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
@@ -25,7 +27,7 @@
             <p>ชื่อ-นามสกุล</p>
             <p>เงินเดือน</p>
             <p>แผนก: ผู้จัดการร้าน</p>
-            <button class="btn green">เพิ่มพนักงาน</button>
+            <button class="btn btn-success">เพิ่มพนักงาน</button>
         </div>
         <br>
         <hr>
